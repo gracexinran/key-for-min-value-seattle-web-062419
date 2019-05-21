f898dc143e2f9fb9fe13f31cbd1c39fe3aae6d6c
Xinran Che <gracexinran@gmail.com>
2019-05-21 06:17:45 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,10 +8,10 @@ def key_for_min_value(name_hash)
   m = name_hash[keys[0]]
 
   name_hash.each do |key, value|
-    if value < min
+    if value <= min
       m = value
     end
   end
-  m
+  
 end
 end
