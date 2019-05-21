ac8c7510c0b59e42a8676c783b2d4fa1f851c892
Xinran Che <gracexinran@gmail.com>
2019-05-21 06:21:33 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
   else
     m = name_hash[keys[0]]
     name_hash.each do |key, value|
-      if value < min
+      if value < m
         m = value
       end
     end
